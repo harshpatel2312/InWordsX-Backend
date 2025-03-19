421e18b5417fb07faa65501af4ace06c481251fe
Harsh Patel <harshpatel2312002@gmail.com>
2025-03-19 15:59:09 -0400

    Implemented /newsModel endpoint

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+# Fetching data from "/newsModel" endpoint
+def fetch_news_model():
+    try:
+        response = requests.get('http://127.0.0.1:5000/newsModel')
+        return response.json() if response.status_code == 200 else {}
+    except requests.exceptions.RequestException as e:
+        print(f"Fetching request failed: {e}")
+        return {}
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():  # put application's code here
+    news_model = fetch_news_model()
+    return render_template("home.html", news_model = news_model)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='127.0.0.1', port=5001, debug=True)
